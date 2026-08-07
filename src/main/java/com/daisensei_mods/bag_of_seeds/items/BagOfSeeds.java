ddcabf5b3959cffd1d29bcfc1458176f7299dd22
大先生 <apb5377@gmail.com>
2026-08-08 00:14:14 +0900

    [add] New Project

---
+++ src/main/java/com/daisensei_mods/bag_of_seeds/items/BagOfSeeds.java
@@ -0,0 +1,15 @@
+package com.daisensei_mods.bag_of_seeds.items;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+public class BagOfSeeds extends BagOfSomething {
+
+    private static final TagKey<Item> seedsTag = ItemTags.create(ResourceLocation.fromNamespaceAndPath("forge", "seeds"));
+
+    public BagOfSeeds(Properties properties) {
+        super(properties, seedsTag);
+    }
+}
